701d6fc71e20efc0902f26ccbf2fcd53a8bfb744
KDanila <concista@gmail.com>
2020-01-16 21:54:35 +0300

    Add sql script and entity(not finished competely) for one to many uni module.

---
+++ OneToManyUni/src/main/java/ru/kdv/hibernate/entity/Course.java
@@ -0,0 +1,30 @@
+package ru.kdv.hibernate.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    public Course(String title) {
+        this.title = title;
+    }
+}
---
+++ OneToManyUni/src/main/java/ru/kdv/hibernate/entity/Review.java
@@ -0,0 +1,22 @@
+package ru.kdv.hibernate.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "review")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "comment")
+    private String comment;
+}
